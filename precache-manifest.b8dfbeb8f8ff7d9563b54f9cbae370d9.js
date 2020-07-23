6df1df5ea2fbb6b5ef607e43284ecef0f25ad07d
Pranay <matepranay11@gmail.com>
2020-07-23 11:25:37 +0530

    Updates

---
+++ precache-manifest.b8dfbeb8f8ff7d9563b54f9cbae370d9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "313dabce7b297fd95566e90e6d5b0bb7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8adc04f456db44570d7e",
+    "url": "/portfolio/static/css/2.80db5df6.chunk.css"
+  },
+  {
+    "revision": "24785ab5fb659848623a",
+    "url": "/portfolio/static/css/main.acc54ba1.chunk.css"
+  },
+  {
+    "revision": "8adc04f456db44570d7e",
+    "url": "/portfolio/static/js/2.7835c079.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.7835c079.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24785ab5fb659848623a",
+    "url": "/portfolio/static/js/main.e9e9c447.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "145ba0f80e3948dae0ee42d669650c96",
+    "url": "/portfolio/static/media/fitness.145ba0f8.jpeg"
+  },
+  {
+    "revision": "c3c3e762141518586d6d3ed4501aa2ea",
+    "url": "/portfolio/static/media/home.c3c3e762.jpeg"
+  },
+  {
+    "revision": "a8dc8c40b1fa3142dc8eb403d976a41a",
+    "url": "/portfolio/static/media/mine.a8dc8c40.jpg"
+  },
+  {
+    "revision": "37b387a86b175c09c5d161520ff2549c",
+    "url": "/portfolio/static/media/netflix.37b387a8.png"
+  },
+  {
+    "revision": "64d3214c5c9b7c2b4eaf7eccc839d3b9",
+    "url": "/portfolio/static/media/trek.64d3214c.jpg"
+  }
+]);
