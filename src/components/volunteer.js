b0fb7eb691dffcc2b8626dba1afcf5ab91b0795b
Pranay <matepranay11@gmail.com>
2020-07-23 20:26:02 +0530

    Add existing file

--- src/components/volunteer.js
+++ src/components/volunteer.js
@@ -20,7 +20,7 @@ function Volunteer() {
     contentArrowStyle={{ borderRight: '7px solid  #3498db' }}
     date="2018 - 2019"
     iconStyle={{ background: 'grey', color: '#fff' }}
-    icon=''
+    icon=<img src="https://img.icons8.com/color/35/000000/conference-foreground-selected.png"/>
   >
     <h3 className="vertical-timeline-element-title">Information Technology Student Association</h3>
     <h4 className="vertical-timeline-element-subtitle">Sports Coordinator</h4>
